6045c88e305d9548d0e4b938e7e60c6ccc618130
sonota88 <yosiot8753@gmail.com>
2020-07-04 15:21:34 +0900

    Call model from main

--- src/main/kotlin/sample/Main.kt
+++ src/main/kotlin/sample/Main.kt
@@ -4,4 +4,13 @@ fun main(rawArgs: Array<String>) {
     println("hi")
     Utils.puts("puts")
     Utils.putskv("putskv", "putskv")
+
+    val model = Model()
+
+    Utils.puts(
+        model.add(
+            Integer.valueOf(rawArgs[0]),
+            Integer.valueOf(rawArgs[1])
+        )
+    )
 }
