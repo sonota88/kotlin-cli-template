0c306e7c983cb2469789b22a5fb4d51eff9b310a
sonota88 <yosiot8753@gmail.com>
2020-07-04 15:25:15 +0900

    Dispatch subcommand

--- src/main/kotlin/sample/Main.kt
+++ src/main/kotlin/sample/Main.kt
@@ -1,16 +1,37 @@
 package sample
 
+import kotlin.system.exitProcess
+
 fun main(rawArgs: Array<String>) {
     println("hi")
     Utils.puts("puts")
     Utils.putskv("putskv", "putskv")
 
+    val args = rawArgs.toList()
+
+    val cmd = args[0]
+    val cmdArgs = Utils.slice(
+        args,
+        1, args.size
+    )
+
     val model = Model()
 
-    Utils.puts(
-        model.add(
-            Integer.valueOf(rawArgs[0]),
-            Integer.valueOf(rawArgs[1])
-        )
-    )
+    when (cmd) {
+        "add" -> {
+            Utils.puts(
+                model.add(
+                    Integer.valueOf(cmdArgs[0]),
+                    Integer.valueOf(cmdArgs[1])
+                )
+            )
+        }
+        "cat" -> {
+            model.cat()
+        }
+        else -> {
+            println("invalid command (${cmd})")
+            exitProcess(1)
+        }
+    }
 }
