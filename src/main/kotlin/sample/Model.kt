b927e3876e065882f4413c121aa458af48a39a0e
sonota88 <yosiot8753@gmail.com>
2020-07-04 15:22:48 +0900

    Add Model#cat

--- src/main/kotlin/sample/Model.kt
+++ src/main/kotlin/sample/Model.kt
@@ -4,4 +4,14 @@ class Model {
     fun add(a: Int, b: Int): Int {
         return a + b
     }
+
+    fun cat() {
+        while (true) {
+            val line : String? = readLine()
+            if (line == null) {
+                break
+            }
+            Utils.puts(line)
+        }
+    }
 }
